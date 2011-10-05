bb06a6c7ce253a09605f12257a3967a79bf986b7
Markus Kohlhase <mail@markus-kohlhase.de>
2011-10-06 00:40:27 +0200

    added simple wrapper for PubSub storage

--- README.markdown
+++ README.markdown
@@ -4,9 +4,14 @@ This repository holds a collection of little helpers for the
 [XMPP](http://xmpp.org/) [Smack](http://www.igniterealtime.org/projects/smack/)
 library. They are written in [Scala](http://www.scala-lang.org/).
 
+Most of the helpers are just wrappers for an easier (but also less powerful)
+API of existing Smack functionalities.
+
+WARNING: They might be incomplete and unstable so be careful ;-)
+
 ## Helpers
 
-### XEP-0133 Service Administration
+### XEP-0133 - Service Administration
 
 [XEP-0133](http://xmpp.org/extensions/xep-0133.html) is a recommendation for
 best practices for service-level administration of servers and components
@@ -30,5 +35,31 @@ serviceAdmin.deleteUser("old@example.org")
 
 serviceAdmin.disableUser("badguy@example.org")
 serviceAdmin.reEnableUser("goodguy@example.org")
+
+```
+
+### XEP-0222/XEP-0223 - Persistent Storage of Public/Private Data via PubSub
+
+[XEP-0222](http://xmpp.org/extensions/xep-0222.html) and
+[XEP-0223](http://xmpp.org/extensions/xep-0223.html)
+define best practices for using the XMPP publish-subscribe
+extension to persistently store semi-public/private data objects.
+
+#### Usage
+
+```scala
+
+val db = new PubSubStorage( connection )
+val storageNode = db.createNode("myNodeId")
+
+val myObj = storageNode.get("myItemId") match {
+
+  case o:Some[MyObjClass] => MyObjClass.fromXmlNode(o)
+  case None               => throw new Exception("could not load object")
+}
+
+myObj.changeSomething()
+
+storageNode.set(myObj.toXmlNode, myObj.getId)
 
 ```
---
+++ src/main/scala/org/smackscalahelpers/PubSubStorage.scala
@@ -0,0 +1,112 @@
+package org.smackscalahelpers
+
+import org.jivesoftware._
+import smack._
+import smackx._
+import smackx.pubsub._
+import smackx.pubsub.FormType
+import smackx.pubsub.LeafNode
+import smackx.packet._        
+import smackx.packet.DataForm
+import scala.collection.JavaConversions._ 
+
+trait Storable{
+
+ def toXML:xml.Node
+ def fromXML(xml:scala.xml.Node):Unit
+
+}
+
+class StorageNode( val name:String, val leafNode:LeafNode){
+
+  import scala.xml._
+
+  /* PRIVATE METHODS*/
+
+  private def updateConfig(priv:Boolean){
+    val cfg = leafNode.getNodeConfiguration() 
+    cfg.setPersistentItems(true)
+    cfg.setDeliverPayloads(false)
+    if (priv == false) 
+      cfg.setAccessModel(AccessModel.open)
+    else
+      cfg.setAccessModel(AccessModel.whitelist)
+    leafNode.sendConfigurationForm(cfg)
+  }
+
+  private def setToNodeMap[T]( set:Set[T] ):Map[String,Node] = 
+    set.asInstanceOf[Set[Item]]
+      .map( x => x.getId() -> x.toXML)
+      .map( x => x._1 -> XML.loadString(x._2).asInstanceOf[Node] )
+      .map( x => x._1 -> x._2.child.head )
+      .toMap
+
+  private def createItem(content:Node, id:String):Node =
+    Utility.trim(<item id={id} >{ content }</item>)
+
+  /* PUBLIC METHODS */
+
+  def set(item:Node){ set(item, "") }
+
+  def set(item:Node, id:String){
+    leafNode.publish(new Item{
+      override def toXML():String = createItem(item, id).toString
+    })}
+
+  def get():Map[String,Node] = 
+    setToNodeMap( leafNode.getItems().toSet )
+
+  def get(id:String):Option[Node] = {
+    val items = get(Set(id))
+    if (items.contains(id))
+      Some(items(id))
+    else None
+  }
+
+  def get(ids:Set[String]):Map[String,Node] =
+    setToNodeMap( leafNode.getItems(ids).toSet )
+
+  def delete(id:String){ leafNode.deleteItem(id) }
+  def delete(ids:Set[String]){ leafNode.deleteItem(ids) }
+  def deleteAll(){ leafNode.deleteAllItems }
+
+  def setToPrivateNode(priv:Boolean = true){ updateConfig(priv) }
+
+}
+
+class PubSubStorage (conn:XMPPConnection){
+
+  private val ACCESS_MODEL  = "pubsub#access_model"
+  private val PERSIST_ITEMS = "pubsub#persist_items"
+  private val cfg = new ConfigureForm(FormType.submit)
+  private val pubSubManager = new PubSubManager(conn)  
+
+  cfg.setPersistentItems(true)
+  cfg.setDeliverPayloads(false)
+  cfg.setAccessModel(AccessModel.whitelist)
+  
+  private def castNodeToStorageNode(node:Node):StorageNode =
+    node match{
+      case l:LeafNode => new StorageNode(node.getId,l)
+      case _          => throw new Exception("could not create node")
+    }
+
+  def createNode(name:String):Option[StorageNode] = {
+    try{
+      Some( castNodeToStorageNode( pubSubManager.createNode(name, cfg)))
+    }catch{
+      case _  => None
+    }
+  }
+
+  def getNode(name:String):Option[StorageNode] = {
+    try{
+      Some(castNodeToStorageNode(pubSubManager.getNode(name)))
+    }catch{
+      case _  => None
+    }
+  }
+
+  def deleteNode(name:String){ pubSubManager.deleteNode(name) }
+
+}
